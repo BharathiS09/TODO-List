459186a0ca1a7bb6215b656c65bb276c86f569a6
Bharathi S <bharathi.d0904@gmail.com>
2026-01-19 19:40:23 +0530

    Update urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TodoListView
+from .views import TodoListView, TodoDetailView
 
 urlpatterns = [
     path('todos/', TodoListView.as_view(), name='todo-list'),
+    path('todos/<int:pk>/', TodoDetailView.as_view(), name='todo-detail'),
 ]
