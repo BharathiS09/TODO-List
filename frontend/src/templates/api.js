2ab5e74eec0d903a9ac2c2fe2c0712a240774281
Bharathi S <bharathi.d0904@gmail.com>
2026-01-19 19:02:36 +0530

    added env in gitignore

--- frontend/src/templates/api.js
+++ frontend/src/templates/api.js
@@ -1,13 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api/todos/";
+const API_URL = `${import.meta.env.VITE_API}/api/todos/`;
 
 export const fetchTodos = () => axios.get(API_URL);
-
 export const createTodo = (todo) => axios.post(API_URL, todo);
-
-export const toggleTodo = (id, todo) =>
-  axios.put(`${API_URL}${id}`, todo);
-
-export const deleteTodo = (id) =>
-  axios.delete(`${API_URL}${id}`);
+export const toggleTodo = (id, todo) => axios.put(`${API_URL}${id}`, todo);
+export const deleteTodo = (id) => axios.delete(`${API_URL}${id}`);
