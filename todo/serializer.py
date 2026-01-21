d37d424c57965472698e54c5a7d69d16573ad24f
Bharathi S <bharathi.d0904@gmail.com>
2026-01-21 19:55:25 +0530

    Update serializer.py

--- todo/serializer.py
+++ todo/serializer.py
@@ -4,4 +4,5 @@ from .models import Todo
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = '__all__'
+        fields = ['id', 'title', 'completed', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at']
